0307352260ed1d5ce15faf2a6c1fd46733be7c5d
ASPIRE\relshaib <raya.elshaib@ww.com>
2023-07-31 02:03:50 +0300

    Add ApiConstants class

---
+++ src/main/java/rest/ApiConstants.java
@@ -0,0 +1,9 @@
+package rest;
+
+public class ApiConstants {
+    public static final String INVALID_EMPTY_URL_ERROR;
+
+    static {
+        INVALID_EMPTY_URL_ERROR = "Invalid or Empty Response";
+    }
+}
