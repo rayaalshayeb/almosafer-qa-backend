c565875f935162d7707d0f7f42c95c9015e11acf
ASPIRE\relshaib <raya.elshaib@ww.com>
2023-07-31 02:00:04 +0300

    Add properties loader to read the config file path

---
+++ src/main/java/helpers/PropertiesLoader.java
@@ -0,0 +1,30 @@
+package helpers;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Properties;
+
+public class PropertiesLoader {
+    public static Properties properties;
+
+    public static Properties readPropertyFile(String configFilePath) {
+        BufferedReader reader;
+        try {
+            reader = new BufferedReader(new FileReader(configFilePath));
+            properties = new Properties();
+
+            try {
+                properties.load(reader);
+                reader.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+            throw new RuntimeException("Configuration.properties not found at " + configFilePath);
+        }
+        return properties;
+    }
+}
